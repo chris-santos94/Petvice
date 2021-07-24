a18724e646da5d643ee8d42fb423861996581786
Stella <stelladev7@gmail.com>
2021-07-24 12:42:19 -0400

    updated js file and html to fit finalized idea

--- index.js
+++ index.js
@@ -1,57 +1,100 @@
 
-/* document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
 
     document.getElementById('form').addEventListener('submit', function(e){
-        e.preventDefault();
-        let foodContainer = document.getElementById('foodContainer');
-        let userSearch = document.getElementById('userSearch').value;
+        e.preventDefault(); //Prevent page from refreshing on submit
         
-        console.log(userSearch);
+    //PET API (axios GET Request)
+        const requiredHeaders = {
+            headers: {
+                "x-rapidapi-key": "9fd45635aamshcd7ceb6aaf23b93p1d29e2jsn2bdeb85da4f1",
+                "x-rapidapi-host": "mlemapi.p.rapidapi.com"
+            }};
         
-        document.getElementById('userSearch').value = '';
+        axios.get("https://mlemapi.p.rapidapi.com/randommlem", requiredHeaders)
+        .then(function(response){
+            //console.log(response.data.url);
+            let petImage = document.getElementById('petImage');
+            petImage.innerHTML = `<img src="${response.data.url}" alt="Random pet image" height=400px width=550>`  
+        })  
         
-        //foodContainer.innerHTML = renderFood();
+        
+    /*ADVICE API (axios GET request)
+        Note: Added the GET request inside of a delay response function bc the quote loads before the image. Trying to get them to load at the same time 
+    */
+        let delayInMilliseconds = 1200; //1.2 second
+        setTimeout(function() {
+        //Code you want to delay goes below
+            axios.get('https://api.adviceslip.com/advice')
+            .then(function(response){
+                //console.log(response.data.slip.advice);
+                let advice = document.getElementById('advice');
+                advice.innerHTML = response.data.slip.advice; 
+            })
+        }, delayInMilliseconds);
 
-    })
-    
-}) */
+        //document.getElementById('userSearch').value = ''; //Clears text from search field
 
-//Test axios b4 copying inside event listener above
-/* axios.get('https://api.yelp.com/v3/businesses/search?Authorization=Bearer CJLKPc0GIPMTvKez02rbHy0bxE5WOkWPTyW69hkHSxUgNpjzsJHfD-HCRl5JX6XJCJYj8ZSrJUe9Uf7j6PvoeYOqOnwYihmQCZrYp4Ge85hOV-PFWaT7urUdiVj3YHYx&location=Atlanta', {
-    headers: {
-       'Access-Control-Allow-Origin': '*'
-    }
-}).then(function(response){
-   
+    }) //End of submit form event listener  
+
+}) //End of DOM content loaded event listener
+
+
+
+/*
+HOW TO GET IMAGE TO LOAD BEFORE THE ADVICE 
+
+1) Build function to get image to generate the image
+  Use Async function to 'await' (slow down) the response. OR, 
+
+2) Use if/else conditional to have advice load after img
+
+
+NOTE:
+Initial code inside of axios.get for loading the image
+Just so you don't forget 
+/* let petImage = document.getElementById('petImage');
+    petImage.innerHTML = `<img src="${response.data.url}" alt="Random pet image" height=400px width=550>`
+
+
+
+*/
+
+
+
+
+
+
+//TRYING FETCH GET REQUEST - Working!
+/* fetch('https://mlemapi.p.rapidapi.com/randommlem', {
+  headers: {
+    'x-rapidapi-key': '9fd45635aamshcd7ceb6aaf23b93p1d29e2jsn2bdeb85da4f1',
+    'x-rapidapi-host': 'mlemapi.p.rapidapi.com'
+  }, 
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
 }); */
 
-axios.get('https://api.yelp.com/v3/businesses/search?Authorization=BearerCJLKPc0GIPMTvKez02rbHy0bxE5WOkWPTyW69hkHSxUgNpjzsJHfD-HCRl5JX6XJCJYj8ZSrJUe9Uf7j6PvoeYOqOnwYihmQCZrYp4Ge85hOV-PFWaT7urUdiVj3YHYx&location=Atlanta');
+
+
+//TRYING OUT AMINE.JS
+
+/* anime({
+    targets: '.square',
+    translateX: 250,
+    rotateZ: 360,
+    scale: 3,
+    duration: 3000,
+    //loop: true
+}); */
 
 
 
-/* function renderFood(restaurantArray){
-    
-    let restaurantsHTML = restaurantArray.map(function(restaurant){
-        
-        return `
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-            </div>
-        `
-    })
-    return restaurantsHTML.join('');
 
-} */
+
+	
